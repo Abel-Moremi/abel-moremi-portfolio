d90634574573fb8c9ab4fd2cc9fa801c27fce892
Abel-Moremi <abelmoremiz@gmail.com>
2020-03-25 23:21:48 +0200

    feat: base layout and logic

---
+++ src/assets/data.js
@@ -0,0 +1,48 @@
+export var main = {
+    name: 'Your Name',
+    mail: 'your.mail@gmail.com',
+    img: 'https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg',
+    role: ['Full Stack Developer'],
+    // Refer https://ionicons.com/ and put the icon name inside iconName key
+    connects: [
+      {
+        name: 'Home',
+        iconName: 'logo-firefox',
+        link: 'https://sivanesh-s.github.io'
+      },
+      {
+        name: 'Github',
+        iconName: 'logo-github',
+        link: 'https://github.com/sivanesh-s'
+      },
+      {
+        name: 'Linkedin',
+        iconName: 'logo-linkedin',
+        link: 'https://www.linkedin.com/in/sivanesh-shanmugam/'
+      },
+      {
+        name: 'StackOverflow',
+        iconName: 'logo-stackoverflow',
+        link: 'https://stackoverflow.com/users/6940525/sivanesh-s'
+      },
+      {
+        name: 'Twitter',
+        iconName: 'logo-twitter',
+        link: 'https://twitter.com/sivanesh_fiz'
+      },
+      {
+        name: 'npm',
+        iconName: 'logo-npm',
+        link: 'https://www.npmjs.com/~sivanesh_s'
+      },
+      {
+        name: 'Mail',
+        iconName: 'mail-outline',
+        link: `mailto:${'yourmail@gmail.com'}?Subject=Hello%20again`
+      }
+    ],
+    links: [
+      { name: 'Projects', link: '#projects' },
+      { name: 'Blogs', link: '#blogs' }
+    ]
+  };
