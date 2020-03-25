e70d77810d708a37f95b72f86d1a91e370105782
Abel-Moremi <abelmoremiz@gmail.com>
2020-03-25 22:33:21 +0200

    fix: blankout the project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Minimal>feaefea </Minimal>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/Minimal.vue'
+import Minimal from './components/Minimal.vue'
 
 export default {
-  name: 'Minimal',
+  name: 'App',
   components: {
-    HelloWorld
+    Minimal
   }
 }
 </script>
