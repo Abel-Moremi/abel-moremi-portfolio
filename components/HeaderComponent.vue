e4e5170e238eef7629c87297b1198558ba28b8ca
Abel Moremi <abelmoremiz@gmail.com>
2021-02-04 22:38:16 +0200

    fix: rearange header to load from component

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,64 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="navbar">
+        <nuxt-link to="/" class="logo">
+          Abel Moremi
+        </nuxt-link>
+        <nav>
+          <ul>
+            <li>
+              <nuxt-link to="/who">
+                who?
+              </nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/who">
+                Posts
+              </nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/who">
+                Contact
+              </nuxt-link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss">
+    header {
+        background: $primary-color;
+        padding: 1em 0;
+        text-align: center;
+
+        .navbar{
+            display: flex;
+            justify-content: space-between;
+            font-weight: bold;
+        }
+
+        a {
+            color: white;
+            text-decoration: none;
+        }
+
+        nav {
+            display: flex;
+        }
+
+        ul {
+            list-style-type: none;
+            padding: 0;
+            display: flex;
+
+            li a {
+            font-weight: normal;
+            padding: .5em;
+            }
+        }
+    }
+</style>
--- components/header_component.vue
+++
@@ -1,5 +0,0 @@
-<template>
-</template>
-
-<style lang="scss">
-</style>
