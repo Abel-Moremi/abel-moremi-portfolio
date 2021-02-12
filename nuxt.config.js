09e62d4494c624283f5528fe3f20408665c8009c
Abel Moremi <abelmoremiz@gmail.com>
2021-02-12 12:50:05 +0200

    add: @nuxt/color-mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,9 @@ export default {
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    // https://color-mode.nuxtjs.org/
+    '@nuxtjs/color-mode'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
