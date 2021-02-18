90d83e3219f9d2e916b0999718f35faa35deac40
Abel Moremi <abelmoremiz@gmail.com>
2021-02-18 15:38:32 +0200

    fix: missing divs

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,6 +82,17 @@ export default {
     linkify: true,
     breaks: true,
     injected: true
+  },
+
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '-mode',
+    storageKey: 'nuxt-color-mode'
   }
 }
 
