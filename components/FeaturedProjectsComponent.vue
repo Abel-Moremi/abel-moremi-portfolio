8e0fcfa34bd4a0a734d77e1166228229867eb054
Abel Moremi <abelmoremiz@gmail.com>
2021-03-01 15:34:56 +0200

    add: initial Content to featured project  Component

---
+++ components/FeaturedProjectsComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="featuredProjects" />
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style  lang="scss">
+
+.featuredProjects{
+    font-family: 'Roboto', sans-serif;
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    align-items: center;
+}
+
+.light-mode {
+  --tech-background: #{ $light-tech-background };
+  --text-color: #{ $light-text-color };
+  --box-shadow: #{ $light-box-shadow };
+}
+
+.dark-mode {
+  --tech-background: #{ $dark-tech-background };
+  --text-color: #{ $dark-text-color };
+  --box-shadow: #{ $dark-box-shadow };
+}
+</style>
