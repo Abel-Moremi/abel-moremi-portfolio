663069607111f5fe3adca91fe902d8f2d2972847
Abel Moremi <abelmoremiz@gmail.com>
2021-03-04 03:11:16 +0200

    add: Content to footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,16 @@
   <div>
     <HeaderComponent />
     <Nuxt />
+    <FooterComponent />
   </div>
 </template>
 <script>
+import FooterComponent from '~/components/FooterComponent.vue'
 import HeaderComponent from '~/components/HeaderComponent.vue'
 export default {
   components: {
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   }
 }
 </script>
